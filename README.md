12e4d7f6eea61ba1e2aa7953055e76e1716f4e1c
Victor Oliveira <xitornet.23@gmail.com>
2023-07-27 20:51:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Electron-Tailwind-React
+ 
---
+++ public/electron.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preloader.js"),
+    },
+  });
+  win.loadURL(isDev? "http://localhost:3000": `file://${path.join(__dirname, "../build/index.html")}`);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
